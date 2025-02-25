2734c9a1d71cd1ee647bbfb103cd9e16b59f1a88
Nishant Raj <nishantanindian@gmail.com>
2025-02-25 23:56:48 +0530

    alien_invasion_v1

---
+++ settings.py
@@ -0,0 +1,19 @@
+
+class Settings:
+    """Class to manage game settings."""
+
+    def __init__(self):
+        """Initialize the game settings."""
+        self.screen_width = 0
+        self.screen_height = 0
+        self.ship_speed = 1.5
+        self.bg_color = (230,230,230)
+
+        #Bullet Settings
+
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_allowed = 3
+        self.bullet_color = (60, 60, 60)
+
